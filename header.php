faf8c8f1d6f384bb2dbc692d970c39f46f3a1b23
Allogo Loic <brownshans@gmail.com>
2022-06-21 12:45:51 +0100

    menu de navigation

---
+++ header.php
@@ -0,0 +1,80 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Papizza | <?= $page; ?></title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+
+            <!-- <nav>
+                <input type="checkbox" id="nav-toogle">
+                <div class="logo"> <a href="#"> <img src="assets/images/papizza/logo_papizza.png" alt="papizza" ></a></div>
+                <ul class="menu">
+                    <li><a href="#"> Nos pizza</a> </li>
+                    <li><a href="#"> Menu</a> </li>
+                    <li><a href="#">Local</a> </li>
+                    <li><a href="#">Passez vos commandes</a> </li>
+                    <li><a href="#">Ouvrez votre franchise</a> </li>
+                    <li><a href="#">Contact</a> </li>
+                   
+                </ul>
+                <label for="nav-toogle" class="icon-burger">
+                    <div class="line"></div>
+                   <div class="line"> </div>
+                </label>
+            </nav>
+             -->
+            <nav class="navbar navbar-expand-lg  ">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/images/papizza/logo_papizza.png" alt="" srcset="">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center  " id="navbarNav">
+                    <ul class="navbar-nav text-uppercase  fw-bold">
+                        
+                        <li class="nav-item ">
+                        <a class="nav-link" aria-current="page" href="#">NOS PIZZA</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">MENU</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">LOCAL</a>
+                        </li>
+                       <li class="nav-item">
+                        <a class="nav-link " href="#">OUVREZ VOTRE FRANCHISE</a>
+                        </li> 
+                        <li class="nav-item">
+                        <a class="nav-link " href="#">PASSEZ VOS COMMANDES</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link " href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                   
+                    </div>
+                    
+                </div>
+                <div class="d-flex connect text-uppercase">
+                       <a href="#" class="log-up">Enregistrement</a>
+                       <a href="#" class="login"> Accéder</a>
+                    </div>
+            </nav>
+        
+        <div class="banner">
+       
+        </div>
+
+  </body>
+
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$page = "Accueil";
+include 'header.php';
+?>
+<!-- 
+<div class="container vh-100">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">   
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+        <img src="assets/images/papizza/slider1.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+        <img src="assets/images/papizza/slider2.jpg" class="d-bloc w-100" alt="...">
+        </div> -->
+        <!-- <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+        </div>
+    </div> -->
+    <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div> 
+</div> -->
+
+
+
+<?php
+include 'footer.php';
+?>
