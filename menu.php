77c044c246fb989fadb99fd96c517b105451a071
Allogo Loic <brownshans@gmail.com>
2022-06-30 18:22:56 +0100

    page menu

--- menu.php
+++ menu.php
@@ -48,13 +48,19 @@ include 'header.php';
         <div class="menu-table-category menu-pizza-au-goût">
             <a href="#" name="pizza-au-goût"></a>
             <div class="menu-category-header">
-                <div class="menu-header-image" style="background-image:url()"></div>
+                <!-- <div class="menu-header-image" style="background-image:url()"></div> -->
                 <div class="menu-category-texts menu-category-pizza-gouter-texts">
                     <div class="menu-category-title menu-category-pizza-gouter-title">ÉPINGLES À GOÛTER</div>
-                    <div class="carta__menu-category-pizza-gouter-description">
-                        <p>La base de la pina est le résultat d'un processus minutieux qui commence par un mélange de farines végétales naturelles de soja, de riz et de blé qui fermentent pendant (rien de plus et rien de moins)
-                            <strong>72 HEURES ! 
-                            , ce qui permet d'obtenir une pâte plus légère, plus croustillante et plus digeste. Et en ajoutant les bons ingrédients, vous obtenez une combinaison imbattable.
+                    <div class="menu-category-pizza-gouter-description">
+                        <p>La base de la pina est le résultat d'un processus
+                         minutieux qui commence par un mélange de farines végétales
+                         naturelles de soja, de riz et de blé qui
+                         fermentent pendant (rien de plus et rien de moins)
+                         <strong>72 HEURES !</strong>, 
+                         ce qui permet d'obtenir une pâte plus légère,
+                         plus croustillante et plus digeste. 
+                         Et en ajoutant les bons ingrédients,
+                         vous obtenez une combinaison imbattable.
                         <br>
                         <br>
                         <strong> 1. Choisissez votre sauce :</strong>
@@ -66,15 +72,192 @@ include 'header.php';
                         <br>
                         Tomate
                         </p>
+                        <p>
+                        <strong>  2. Sélectionnez tous les ingrédients :</strong>
+                        </p>
+                        <p>
+                        Poulet rôti
+                        <br>
+                        Ananas   
+                        <br>
+                        Piment rouge
+                        <br>
+                        lard fumé 
+                        <br>
+                        Champignons 
+                        <br>
+                        Chorizo épicé ​
+                        <br>
+                        Fromage bleu
+                        <br>
+                        oignon rouge
+                        <br>
+                        Thon
+                        <br>
+                        JAMBON YORK
+                        </p>
+                        <p><strong>3. Donnez-lui une touche :</strong></p>
+                        <p>
+                        ORIGAN
+                        <br>
+                        HUILE ÉPICÉE    
+                        <br>
+                        SALSA BBQ (+0,5dhs)
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="menu-table-category menu-pizza-au-goût"></div>
-        <div class="menu-table-category menu-pizza-au-goût"></div>
-        <div class="menu-table-category menu-pizza-au-goût"></div>
-        <div class="menu-table-category menu-pizza-au-goût"></div>
+        <div class="menu-table-category menu-pizza-au-tribune">
+                <a href="#" name="pizza-au-tribune"></a>
+                <div class="menu-category-header-pizzas-au-tribune">
+                <div class="menu-cabecera-image-au-tribune" style="background-image: url('assets/images/papizza/storage.png')"></div>
+                <div class="menu-category-texts menu-category-pizzas-au-tribune-texts">
+                    <div class="menu-category-pizzas-au-tribune-title">ÉPINGLES</div>
+                    <div class="menu-category-pizzas-au-tribune-description">
+                        <p>Pinsa artisanale légère, croquante et digestive, avec plus de 72 heures de fermentation.</p>
+                        <table border=0; width="90%"  cellspacing="5" cellpadding="5">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        Médian:
+                                        <strong> 8,90dh</strong>
+                                    </td>
+                                    <td>
+                                        Prime moyenne:
+                                        <strong> 11,89dh</strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Familier:
+                                        <strong> 14,90dh</strong>
+                                    </td>
+                                    <td>
+                                        Prime familiale:
+                                        <strong>17,90dh</strong>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="menu-table-category menu-pâte-épaisse">
+            <a href="#" name="pâte-épaisse"></a>
+            <div class="menu-category-header-pâte-épaisse">
+                <div class="menu-cabecera-image-pâte-épaisse" style="background-image: url('https://www.papizza.es/storage')"></div>
+                <div class="menu-category-pâte-épaisse-texts">
+                    <div class="menu-category-pâte-épaisse-title">ÉPINGLES</div>
+                </div>
+            </div>
+            <ul class="menu-produits-pâte-épaisse menu-produits">
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">Marguerite</div>
+                    <div class="menu-produit-description">Base de notre savoureuse sauce tomate, prosciutto, mozzarella râpée, huile d'olive vierge et une touche d'origan.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">Prosciutto</div>
+                    <div class="menu-produit-description">Base de notre savoureuse sauce tomate, mozzarella râpée, huile d'olive vierge et une touche d'origan.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">CARNISSIMA</div>
+                    <div class="menu-produit-description">AVEC SAUCE TOMATE, MOZZARELLA, HUILE D'OLIVE, ORIGAN, LANIÈRES DE POULET RÔTIES, JAMBON CUIT, CHAMPIGNONS LAMINÉS, SAUCE BARBECUE, BŒUF HACHÉ.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">DIAVOLA</div>
+                    <div class="menu-produit-description">AVEC SAUCE TOMATE, MOZZARELLA, SAUCISSE ÉPICÉE, BŒUF HACHÉ ET OIGNON ROUGE.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">HAWAII</div>
+                    <div class="menu-produit-description">AVEC SAUCE TOMATE, MOZZARELLA, JAMBON CUIT ET ANANAS.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">CARBONARA</div>
+                    <div class="menu-produit-description">AVEC MOZZARELLA, SAUCE CARBONARA, BACON TRANCHÉ ET CHAMPIGNONS LAMINÉS.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">BACON 2.0</div>
+                    <div class="menu-produit-description">AVEC SAUCE TOMATE, MOZARRELLA, BACON CROUSTILLANT, BACON TRANCHÉ, CHAMPIGNONS LAMINÉS.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">QUATRE FROMAGES</div>
+                    <div class="menu-produit-description">AVEC MOZZARELLA, SAUCE AU FROMAGE, FROMAGE BLEU ET FROMAGE DE CHÈVRE.</div>
+                </li>
+                <li class="menu-produits-pâte-épaisse produits-pâte-épaisse">
+                    <div class="menu-produit-title">NOUVELLE ITALIE</div>
+                    <div class="menu-produit-description">CON SALSA DE TOMATE, MOZZARELLA, TIRAS DE POLLO ASADO, CEBOLLA BLANCA, PIMIENTO ROJO Y SALSA BARBACOA.</div>
+                </li>
+
+            </ul>
+           
+        </div>
+        <div class="menu-table-category menu-pâte-fine">
+            <a href="#" name="pâte-fine"></a>
+            <div class="menu-category-header-pâte-fine">
+                <div class="menu-category-pâte-fine-texts">
+                    <div class="menu-category-pâte-fine-title">PINCES PREMIUM</div>
+                    
+                </div>
+                
+                <ul class="menu-produits-pâte-fine menu-produits">
+                    <li class=" produits-pâte-fine">
+                        <div class=" produits-pâte-fine-title">César & Parmesan</div>
+                        <div class=" produits-pâte-fine-description">BASE DE SAUCE CÉSAR CRÉMEUSE, OIGNONS CARAMÉLISÉS, BACON CROUSTILLANT, POULET RÔTI ET ROQUETTE DE LA JUNGLE, FINI AVEC DES TRANCHES DE PARMESAN.</div>
+                    </li>
+                    <li class=" produits-pâte-fine">
+                        <div class=" produits-pâte-fine-title">TRUFFE & PROSCIUTTO</div>
+                        <div class=" produits-pâte-fine-description">BASE DE CRÈME, HARMONIE DE TRUFFES, CROUSTILLES DE VIANDE, FROMAGE BLEU ET PROSCIUTTO, TERMINÉ AVEC DE L'ORIGAN PIPIRRANA.</div>
+                    </li>
+                    <li class=" produits-pâte-fine">
+                        <div class=" produits-pâte-fine-title">ROQUETTE et CERISE</div>
+                        <div class=" produits-pâte-fine-description">BASE DE SAUCE TOMATE, BOCCONCIN MOZZARELLA, JUNGLE ROCKET, TOMATE CERISE ET ROULEAU AU FROMAGE DE CHÈVRE, FINI AVEC CRÈME DE VINAIGRE BALSAMIQUE.</div>
+                    </li>
+                    <li class=" produits-pâte-fine">
+                        <div class=" produits-pâte-fine-title">ROQUETTE et CERISE</div>
+                        <div class=" produits-pâte-fine-description">BASE DE SAUCE CÉSAR CRÉMEUSE, OIGNONS CARAMÉLISÉS, BACON CROUSTILLANT, POULET RÔTI ET ROQUETTE DE LA JUNGLE, FINI AVEC DES TRANCHES DE PARMESAN.</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="menu-table-category menu-starters">
+            <a href="#" name="starters"></a>
+            <div class="menu-category-header-starters">
+                <div class="menu-category-starters-texts">
+                    <div class="menu-category-starters-title">Entrées</div>
+                    <div class="menu-category-starters-descritpion">
+                        <p><strong> EMPANADILLA DE LONGE</strong> : AVEC UNE TOUCHE LÉGÈREMENT ÉPICÉE.</p>
+                        <p><strong> CAPRESE EMPANADILLA </strong> : AU FROMAGE MOZZARELLA AVEC UNE TOUCHE DE ROQUETTE.</p>
+                        <p><strong> PÂTISSERIE AU POULET</strong> : AVEC UNE TOUCHE DE CURRY.</p>
+                        <p><strong> PIZZATATAS AU BACON ET AU FROMAGE</strong> : GAJO DE POMMES DE TERRE AU FOUR, BAIN DE SAUCE AU FROMAGE ET BACON CROUSTILLANT.</p>
+                        <p><strong> PIZZATAS À LA VIANDE ET AU BARBECUE</strong> : GAJO DE POMMES DE TERRE AU FOUR, AVEC DU BŒUF HACHÉ ET PÂTÉ À LA SAUCE BBQ..</p>
+                        <p><strong> MOZZARELLA ET ROCKET CALZONE </strong> : UN CLASSIQUE ITALIEN.</p>
+                        <p><strong> YORK AND CHEESE CALZONE</strong> :  UNE BASE ITALIENNE.</p>
+                        <p><strong> CHICKEN ALÍSSIMAS</strong> : NOS AILES DE POULET, AVEC DU BŒUF HACHÉ ET BAIN DE SAUCE BBQ..</p>
+                        <p><strong> NACHOLINIS</strong> : COMMENT SONNENT LES NACHOS, AVEC MOZZARELLA, POIVRON ROUGE, SAUCE CÉSAR ET BŒUF HACHÉ, CUIT AU FOUR.</p>
+
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="menu-table-category menu-desserts">
+            <a href="#" name="desserts"></a>
+            <div class="menu-category-header-desserts">
+                <div class="menu-category-desserts-texts">
+                    <div class="menu-category-desserts-title">DESSERTS</div>
+                    <div class="menu-category-desserts-descritpion">
+                        <p>Finir par un bon dessert est essentiel. Consultez nos propositions et clôturez votre dîner avec une saveur incomparable comme nos crêpes, glaces ou coupes de mousse.</p>
+                       
+                    </div>
+                   
+                </div>
+            </div>
+            <strong>
+        </div>
     </div>
+   
 </div>
 
 
